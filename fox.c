f93562d88b2a2d7c8ec647cdbcd2b33c5036e82c
P-Silva1974746 <pedrofsilva5164@gmail.com>
2025-10-28 23:12:08 +0000

    ADD: Fox logic implmented

--- fox.c
+++ fox.c
@@ -37,7 +37,8 @@ void fox_min_plus(int N, int block_size, int Q, int **A, int **C, MPI_Comm comm)
     int **A_local = allocate_matrix(block_size);
     int **B_local = allocate_matrix(block_size);
     int **C_local = allocate_matrix(block_size);
-
+    int **A_temp = allocate_matrix(block_size);
+    
     //inicializa C_local com INF 
     for (int i = 0; i < block_size; i++)
         for (int j = 0; j < block_size; j++)
@@ -49,12 +50,38 @@ void fox_min_plus(int N, int block_size, int Q, int **A, int **C, MPI_Comm comm)
 
     //proximos passos:
     //executar fases do fox (estive a tentar fazer mas deu-me dor de cabeça ainda estou a tentar perceber), recorrendo a funçao min plus multiply em matrix.c
+
+    for (int iter; iter=0; iter++){
+        int root_in_row = (row+ iter)%Q; 
+
+        // Sender process
+        if(col=root_in_row){
+            MPI_Bcast(&A_local[row][col], block_size*block_size, MPI_INT, root_in_row, row_comm);
+        }else{
+            //receiving processes
+            MPI_Bcast(&A_temp[row][col], block_size*block_size, MPI_INT, root_in_row, row_comm);
+        }
+
+        // This lines makes it so that the pointer A_used is equal to A_local or A_temp depending on the condition 
+        int **A_used = (col == root_in_row) ? A_local : A_temp;
+
+        // Multiplication operation
+        min_plus_multiply(block_size, A_used, B_local, C_local);
+
+        // B up shift
+        int scr, dest;
+        MPI_Cart_shift(col_comm, 0, -1, &scr, &dest);
+        MPI_Sendrecv_replace(&B_local[0][0], block_size*block_size, MPI_INT, dest, 0, scr, 0, col_comm, MPI_STATUS_IGNORE);
+    }
+    
+
+
     //no final, a submatriz C está com os valores finais
 
     free_matrix(A_local);
     free_matrix(B_local);
     free_matrix(C_local);
-
+    free_matrix(A_temp);
     MPI_Comm_free(&row_comm);
     MPI_Comm_free(&col_comm);
     MPI_Comm_free(&grid_comm);
